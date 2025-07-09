e2abb328dc7a59551cb738b80f77c6c0f1f55a4f
Julian Bueno <julian.bueno@gmail.com>
2025-07-09 19:55:36 +0000

    added file reader

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::io::{BufRead, BufReader};
+use std::env;
+
+fn read_file(path: &str) {
+    let file = File::open(path);
+    let file = match file {
+        Ok(file) => file,
+        Err(error) => {
+            match error.kind() {
+                std::io::ErrorKind::NotFound => {
+                    panic!("File not found: {}", error)
+                }
+                _ => {
+                    panic!("Error opening file: {}", error)
+                }
+            }
+        }
+    };
+    
+    let reader = BufReader::new(file);
+    for line in reader.lines() {
+        match line {
+            Ok(line) => println!("{}", line),
+            Err(error) => {
+                panic!("Error reading line: {}", error)
+            }
+        }
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // The first argument is the path that was used to call the program.
+    println!("My path is {}.", args[0]);
+    read_file(&args[0]);
+    
+}
